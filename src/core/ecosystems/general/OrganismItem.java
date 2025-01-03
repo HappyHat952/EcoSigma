3ea2c4b87e085133b1ab9ddcc647995827073ca6
Nitarara <senrenjp@gmail.com>
2025-01-03 06:54:43 -0500

    popups are actually different

--- src/core/ecosystems/general/OrganismItem.java
+++ src/core/ecosystems/general/OrganismItem.java
@@ -1,4 +1,35 @@
 package core.ecosystems.general;
 
+import core.ecosystems.Grid;
+import org.newdawn.slick.Graphics;
+
 public class OrganismItem {
+    protected int x;
+    protected int y;
+    protected int i;
+    protected int numOrganisms;
+    protected Grid grid;
+
+    protected Class<? extends Organism> myOrganism;
+
+    protected static int buffer;
+    protected static int margin;
+
+    public OrganismItem(int i , Class<? extends Organism> myOrganism, Grid grid)
+    {
+
+    }
+    public void render(Graphics g)
+    {
+        //draws square if none available or red animal image
+
+    }
+
+    public void action() {
+        if (numOrganisms >0)
+        {
+            grid.addOrganism(myOrganism);
+            numOrganisms --;
+        }
+    }
 }
